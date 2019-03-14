db1822de0c0c644d485580ed479228bf8a7c406b
Yevheniia Dilekoglu <jene4ka1818@gmail.com>
2019-03-14 11:31:04 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 
+//BUTTONS ANIMATION=======================================================================================
 var colors = new Array(
     [62,35,255],
     [60,255,60],
@@ -18,16 +19,13 @@ var colors = new Array(
   //transition speed
   var gradientSpeed = 0.002;
   
-  function updateGradient()
-  {
-    
-    if ( $===undefined ) return;
-    
+  function updateGradient(){
+      if ( $===undefined ) return;
   var c0_0 = colors[colorIndices[0]];
   var c0_1 = colors[colorIndices[1]];
   var c1_0 = colors[colorIndices[2]];
   var c1_1 = colors[colorIndices[3]];
-  
+        
   var istep = 1 - step;
   var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
   var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
@@ -41,7 +39,7 @@ var colors = new Array(
   
    $('#gradient').css({
      background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
     
     step += gradientSpeed;
     if ( step >= 1 )
@@ -60,23 +58,23 @@ var colors = new Array(
   
   setInterval(updateGradient,10);
 
-
-    /*var count = 0;
-    var images = ['../GifTastic/assets/images/whitecat.jpg',
-                  '../GifTastic/assets/images/beardeddragon1.jpg',
-                  '../GifTastic/assets/images/catsorange.jpg', 
-                  '../GifTastic/assets/images/rainbowkitty.jpg',
-                  '../GifTastic/assets/images/iguana1.jpg',
-                  '../GifTastic/assets/images/dog1.jpg',
-                  '../GifTastic/assets/images/chameleon1.jpg',
-                  '../GifTastic/assets/images/iguana2.jpg',
-                  '../GifTastic/assets/images/dog2.jpg',
-                  '../GifTastic/assets/images/beardeddragon2.jpg',
-                  '../GifTastic/assets/images/chameleon2.jpg',
-                  '../GifTastic/assets/images/beardeddragon3.jpg'];
+//SLIDING BACKGROUND=================================================================================================
+    var count = 1;
+    var images = ['../GifTasticDone/assets/images/whitecat.jpg',
+                  '../GifTasticDone/assets/images/beardeddragon1.jpg',
+                  '../GifTasticDone/assets/images/catsorange.jpg', 
+                  '../GifTasticDone/assets/images/rainbowkitty.jpg',
+                  '../GifTasticDone/assets/images/iguana1.jpg',
+                  '../GifTasticDone/assets/images/dog1.jpg',
+                  '../GifTasticDone/assets/images/chameleon1.jpeg',
+                  '../GifTasticDone/assets/images/iguana2.jpg',
+                  '../GifTasticDone/assets/images/dog2.jpg',
+                  '../GifTasticDone/assets/images/beardeddragon2.jpg',
+                  '../GifTasticDone/assets/images/chameleon2.jpg',
+                  '../GifTasticDone/assets/images/beardeddragon3.jpg'];
     var image = $(".fader");
   
-    image.css("background-image","url("+images[count]+")");
+    image.css("background-image","url("+images[0]+")");
   
     setInterval(function(){
       image.fadeOut(700, function(){
@@ -87,48 +85,42 @@ var colors = new Array(
       {
         count = 0;
       }
-    },40000);
-  */
+    },4000);
+//ACTUALL FUNCTIONALITY CODE STARTS HERE========================================================================
 
-
-
-
-   
-   var topics = ['Axolotl', 'Bearded Dragon', 'Snake', 'Cats', 'Dogs', 
+//Array with buttons names
+var topics = ['Axolotl', 'Bearded Dragon', 'Snake', 'Cats', 'Dogs', 
                   'Hamsters', 'Flamingos', 'Tigers', 'Iguanas', 'Bears', 'Rats'];
   
-
+//Creating buttons
 var createBtn = function(){
-    $('#buttons')
+  $('#buttons')
     for(var i = 0; i<topics.length; i++){
-        var newBtn = $('<a href="#" class = "btn btn-sm animated-button victoria-one"></a>');
+        var newBtn = $('<a href="#" class = "btn btn-sm animated-button victoria-one col-md-4 col-sm-3 col-xs-2"></a>');
  
          newBtn.attr('data-name',topics[i]);
-            newBtn.attr('value', topics[i])
+         newBtn.attr('value', topics[i])
          newBtn.text(topics[i]);
-         $('#buttons').append(newBtn);
+  $('#buttons').append(newBtn);
+
          console.log(i)
          console.log(newBtn)
     }
 }
-
-$('#gifInputBtn').on('click', function(event){
+//Function that clear buttons div to prevent repeating buttons
+$('#InputBtn').on('click', function(event){
     $('#buttons').empty()
 
     event.preventDefault();
-    var addedBtn = $("#gifInputText").val()
-   
+     
+    var addedBtn = $("#InputText").val()
+   //Adding new button(user button)
      topics.push(addedBtn);
      createBtn()
-     
-     
-
-
-
 })
 createBtn()
 
-
+//Display gif
 function displayGif(){
 var gifSearch = $(this).attr("data-name");
 var queryURL = "https://api.giphy.com/v1/gifs/search?q="+gifSearch+"&api_key=COGiXXultfkA9osXS98VBU5pt56bImum&limit=10"
@@ -141,29 +133,24 @@ $.ajax({
     var gifResponse = response.data
   
     for (var i=0; i < gifResponse.length; i++){
-        //$(".gifDisplayImg").append("<img class= 'newGif' src ='"+gifResponse[i].images.original_still.url+"'/>")
-        //var gifDisplay = $('#gifDisplayImg')
-        var gifDisplay = $('<div class= gifDisplayText>')
-        var onClickGif = $('<img>')
+        var gifDisplay = $('<div class ="gifDisplayText">')
+        var onClickGif = $('<img class = "col col-md-12 col-sm-4 col-xs-1">')
             onClickGif.attr('src', gifResponse[i].images.original_still.url)
             onClickGif.attr('title', "Rating: "+ gifResponse[i].rating)
-        //onClickGif.attr('src', gifResponse[i].images.original_still.url)
             onClickGif.attr('data-still', gifResponse[i].images.original_still.url);
             onClickGif.attr('data-state', 'still');
             onClickGif.addClass('gif')
             onClickGif.attr('data-animate', gifResponse[i].images.original.url);
-        gifDisplay.append(onClickGif)
-        $('#gifDisplayImg').append(gifDisplay)
-    }
+            gifDisplay.append(onClickGif)
+   $('#gifDisplayImg').append(gifDisplay)
+};
     
-    $('#removeGifsText').on('click', function(){
-        $(".gifDisplayText").fadeOut()
+   $('#removeGifsText').on('click', function(){
+      $(".gifDisplayText").fadeOut()
     })
-   
-})
+  })
 }
-
-
+//Gif play and stop
 $(document).on('click', '.gif', function(){
 	var state = $(this).attr('data-state');
 		if ( state == 'still'){
@@ -172,10 +159,8 @@ $(document).on('click', '.gif', function(){
             }else{
                 $(this).attr('src', $(this).data('still'));
                 $(this).attr('data-state', 'still');
-                
             };
             console.log(this)
-                   
 });
 $(document).on('click', '.btn', displayGif)
 
@@ -184,6 +169,7 @@ $(document).on('click', '.btn', displayGif)
 
 
   
+
 
 
 
